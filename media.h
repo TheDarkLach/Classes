504b32fdad20222ba9c5a45eccae705d37c5e6dd
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2020-10-23 11:50:43 -0700

    Update media.h

--- media.h
+++ media.h
@@ -1,20 +1,19 @@
-#ifndef media_h
-#define media_h
+#ifndef MEDIA_H
+#define MEDIA_H
 
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
-class Media {
+class Media{
  public:
   Media();
-  int* getYear(); //pointing to year
-  char* getTitle(); //pointing to title
-  virtual int getType(); //storing type
+  char* getTitle();
+  int* getYear();
+  virtual int getType();
  private:
-  int year; //storing year
-  char title[100]; //storing title
+  char title[30];
+  int year;
 };
-
 #endif
