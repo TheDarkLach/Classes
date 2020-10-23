32578f972424f7826d0f9b588dcfb25d7278e59c
TheDarkLach <11thunderstorm11@gmail.com>
2020-10-23 08:47:50 -0700

    main

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  vector<Media*>* AllMedia = new vector<Media*>;
+  bool isRunning = true;
+  while (isRunning)
+  {
+    char input[15];
+    
+    cin >> input;
+    else if (strcmp(input,"QUIT") == 0)
+    {
+      cout << "You have quit\n";
+      isRunning = false;
+    }
+    else
+    {
+      cout << "Type in one of the commands prompted: ADD, DELETE";
+    }
+  }
+}
