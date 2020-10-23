a599f203426e768b5991ed3128eaeb4beeb2c945
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2020-10-23 10:09:18 -0700

    media.cpp
    fix media cpp and header

--- media.cpp
+++ media.cpp
@@ -1,33 +1,22 @@
-#include <stdio.h>
 #include <iostream>
 #include <cstring>
-
 #include "media.h"
-#include "game.h"
 
 using namespace std;
 
-Media::Media(){
-	cout << "Media created";
-};
+Media::Media() {//calling .h file
 
-int getType(){
+
+}
+
+int Media::getType() {//sending type to .h
   return 0;
 }
 
-char* getTitle(){
+int* Media::getYear() {//sending year to .h
+  return &year;
+}
+
+char* Media::getTitle() {//sending year to .h
   return title;
-  //Same as return &title[0]
-}
-
- int getYear(){
-  return year;
-}
-
-void setYear(int newYear){
-  year = newYear;
-}
-
-void setTitle(char* newTitle){
-  strcpy(title,newTitle);
 }
